8ad7804178639559a1c5e16206f4053ca5f3af38
Pyettra <pepepyettra@gmail.com>
2026-09-10 14:41:56 -0300

    atualizacao

--- agenda_firebase/lib/main.dart
+++ agenda_firebase/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'tela_principal.dart';
 
+/// oiii
+
 Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
